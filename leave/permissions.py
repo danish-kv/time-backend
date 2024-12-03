94abe07127c858c3f1f3882dc66f20ff1582dd76
Danish <muhammeddanishkv05@gmail.com>
2024-12-03 11:03:33 +0530

    fix: correct custom permission, added static and media url

--- leave/permissions.py
+++ leave/permissions.py
@@ -8,5 +8,5 @@ class IsManager(BasePermission):
 
 class IsEmployee(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'empolyee'
+        return request.user.is_authenticated and not request.user.is_superuser
     
